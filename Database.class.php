aa6be52afc3641fc332092d52fb52f0cc2ebc52b
Jees K Denny <jees.jees5@gmail.com>
2016-12-28 21:10:09 +0530

    Database.class.php Added

---
+++ Database.class.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+* Databsae Config
+*/
+class Database
+{
+	private $conn;
+	private $host;
+	private $user;
+	private $pass;
+	private $databaseName;
+	private $port;
+	private $debug;
+	
+	function __construct()
+	{
+		$this->conn = false; 
+		$this->host = 'localhost'; //hostname
+		$this->user = 'root'; //username
+		$this->pass = ''; //password
+		$this->databaseName = 'dbname'; //name of your database
+		$this->port = '3306'; //port 
+		$this->debug = true;
+		$this->connect();
+	}
+
+	public function connect() 
+	{
+		if (!$this->conn) 
+		{
+			$this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->databaseName);
+			mysqli_set_charset($this->conn,"utf8");
+			if (!$this->conn) 
+			{
+				if($this->conn->connect_errno > 0)
+				{
+			    	die('Unable to connect to database [' . $this->conn->connect_error . ']');
+				}
+			} 
+		}
+		return $this->conn;
+	}
+
+	public function disconnect()
+	{
+	    if($this->conn)
+	    {
+	        if(mysqli_close($this->conn))
+	        {
+	            $this->conn = false; 
+	            return true; 
+	        }
+	        else
+	        {
+	            return false; 
+	        }
+	    }
+	}
+
+	public function prepare($value)
+	{
+		return $this->conn->prepare($value);
+	}
+
+ 	public function insert_id(){
+        return $this->conn->insert_id;
+    }
+
+    public function affected_rows()
+    {
+    	return $this->conn->affected_rows;
+    }
+}
