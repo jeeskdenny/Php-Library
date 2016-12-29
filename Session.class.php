ec042963e9d64ea4ffa69c5702c0f4158d983763
Jees K Denny <jees.jees5@gmail.com>
2016-12-29 10:40:01 +0530

    Session.class added

---
+++ Session.class.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+* Session Data Handle 
+* Call This Class 
+*
+*	require_once 'Session.php';
+*	Session::start();
+*	Session::set('best','test');
+*	Session::set('name',Array('name'=>'jees','number'=>'9633'));
+*
+*	echo Session::get('name','number');
+*	Session::destroy();
+*	Session::display();
+*	
+* 	@author  Jees K Denny
+* 	@version 1.0, 28/12/16
+* 	@since   28/12/16
+*/
+
+class Session
+{	
+	private static $isSessionStart = false;
+
+	public static function start()
+	{
+		if(self::$isSessionStart == false)
+		{
+			session_start();
+			self::$isSessionStart = true;
+		}
+		
+	}
+	
+	public static function set($key, $value)
+	{
+		$_SESSION[$key]=$value;
+	}
+
+	public static function get($key, $secondKey= false)
+	{
+		if($secondKey==true){
+
+			if(isset($_SESSION[$key][$secondKey]))
+				return $_SESSION[$key][$secondKey];
+
+		}else{
+
+			if(isset($_SESSION[$key]))
+			return $_SESSION[$key];
+
+		}
+
+		return false;
+
+	}
+
+	public static function display()
+	{
+		echo '<pre>';
+		print_r($_SESSION);
+		echo '</pre>';
+	}
+
+	public static function destroy($key, $value)
+	{
+		if(self::$isSessionStart==true){
+			session_unset();
+			session_destroy();
+		}
+	}
+
+}
